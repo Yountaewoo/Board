8277c06faec3948ce812248414c48fb718349e9f
YounTaeWoo <tty7797@naver.com>
2024-12-20 15:35:03 +0900

    feat:Board,Comment,Post 생성

---
+++ src/main/java/yountaewoo/Board/Post.java
@@ -0,0 +1,69 @@
+package yountaewoo.Board;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class Post {
+    private String title;
+    private String content;
+    private List<Comment> comments;
+    private Board board;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+
+    protected Post() {
+
+    }
+
+    public Post(Board board, List<Comment> comments, String content, Long id, String title) {
+        this.board = board;
+        this.comments = comments;
+        this.content = content;
+        this.id = id;
+        this.title = title;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return Objects.equals(title, post.title) && Objects.equals(content, post.content) && Objects.equals(comments, post.comments) && Objects.equals(board, post.board) && Objects.equals(id, post.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, content, comments, board, id);
+    }
+}
+
+
+
